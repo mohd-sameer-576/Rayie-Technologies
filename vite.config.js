3b9c970be25bc86d9280541bf721d01999b3207c
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2026-02-04 23:12:40 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Rayie-Technologies',
+  base: '/rayie-technologies',
 })
